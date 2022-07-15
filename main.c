0aba914dd933ec959c1165ad3e26c0f57c5dc4ed
Detjon <detjon.mataj@gmail.com>
2022-07-16 01:44:16 +0200

    Implemented simple perceptron training algorithm

--- main.c
+++ main.c
@@ -1,129 +1,144 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <errno.h>
 #include <math.h>
-#include <assert.h>
-#include <time.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <limits.h>
-#include <errno.h>
 
 #define WIDTH 50
 #define HEIGHT 50
 #define PPM_MAX_COLOR 255
 #define PPM_MIN_COLOR 0
-#define NUMBER_OF_SAMPLES 15
+#define NUMBER_OF_SAMPLES 100
 #define SAMPLES_FOLDER_NAME "samples"
 #define RECTANGLE_SAMPLE_NAME "rectangle"
+#define WEIGHT_SAMPLE_NAME "weight"
 #define CIRCLE_SAMPLE_NAME "circle"
-#define BORDER_OFFSET_L_T 10
-#define BORDER_OFFSET_R_B 10
+#define BORDER_OFFSET_L_T 0 // left and top
+#define BORDER_OFFSET_R_B 0 // right and bottom
+#define BIAS 10.0
+#define FORWARD_TRAIN_PASSES 100
+#define CHECK_SEED_VALUE 666
+#define TRAIN_SEED_VALUE 69
+#define SAVE_TO_SAMPLES_FOLDER 1
+#define PPM_RANGE 10
 
 #define PPM_SCALE_FACTOR 25
 #define scale(x) ((x)*PPM_SCALE_FACTOR)
 #define unscale(x) ((x) / PPM_SCALE_FACTOR)
 #define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 typedef float Layer[HEIGHT][WIDTH];
+
+///* Generate random value between range min and max
+int random_int_range(int min, int max)
+{
+    assert(min < max);
+    return min + rand() % (max - min);
+}
 
 // Save the layer to a ppm file
 void save_layer_ppm(Layer layer, const char *filename)
 {
-	FILE *fp = fopen(filename, "wb");
-	if (fp == NULL)
-	{
-		///* %m will print the error message from strerror() supplied by errno
-		///* Such as "No such file or directory"
-		///* %m is only supported by GCC (GNU Compiler Collection)
-		///? fprintf(stderr, "Error opening file %s: %m\n", filename);
+#if SAVE_TO_SAMPLES_FOLDER
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL)
+    {
+        ///* %m will print the error message from strerror() supplied by errno
+        ///* Such as "No such file or directory"
+        ///* %m is only supported by GCC (GNU Compiler Collection)
+        ///? fprintf(stderr, "Error opening file %s: %m\n", filename);
 
-		///* Print the error message from strerror() supplied by errno
-		fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
-		exit(1);
-	}
+        ///* Print the error message from strerror() supplied by errno
+        fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
+        exit(1);
+    }
 
-	///* Header of the ppm file
-	fprintf(fp, "P6\n");
-	fprintf(fp, "%d %d\n", scale(WIDTH), scale(HEIGHT));
-	fprintf(fp, "255\n");
+    ///* Header of the ppm file
+    fprintf(fp, "P6\n%d %d 255\n", scale(WIDTH), scale(HEIGHT));
 
-	///* Write the layer to the file
-	for (int y = 0; y < scale(HEIGHT); ++y)
-	{
-		for (int x = 0; x < scale(WIDTH); ++x)
-		{
-			float scaler = layer[unscale(y)][unscale(x)];
-			///* Creating a blue color pixel from the scaler
-			unsigned char pixel[3] = {0, 0, (unsigned char)scale(scaler * PPM_MAX_COLOR)};
-
-			///* Write the pixel to the file
-			fwrite(pixel, sizeof pixel, 1, fp);
-		}
-	}
-
-	///* Close the file
-	fclose(fp);
+    ///* Write the layer to the file
+    for (int y = 0; y < scale(HEIGHT); ++y)
+    {
+        for (int x = 0; x < scale(WIDTH); ++x)
+        {
+            float scaler = (layer[unscale(y)][unscale(x)] - PPM_RANGE) / (2.0f * PPM_RANGE);
+            char red = random_int_range(0, 80);
+            char green = (char)floorf(PPM_MAX_COLOR * (1.0f - scaler));
+            char blue = (char)floorf(scaler * PPM_MAX_COLOR);
+            char pixel[3] = {red, green, blue};
+            ///* Write the pixel to the file
+            fwrite(pixel, sizeof(pixel), 1, fp);
+        }
+    }
+    ///* Close the file
+    fclose(fp);
+#endif
 }
 
 ///* Save the layer to a binary file
 void save_layer_bin(Layer layer, const char *filename)
 {
-	FILE *fp = fopen(filename, "wb");
-	if (fp == NULL)
-	{
-		///* %m will print the error message from strerror() supplied by errno
-		///* Such as "No such file or directory"
-		///* %m is only supported by GCC (GNU Compiler Collection)
-		///? fprintf(stderr, "Error opening file %s: %m\n", filename);
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL)
+    {
+        ///* %m will print the error message from strerror() supplied by errno
+        ///* Such as "No such file or directory"
+        ///* %m is only supported by GCC (GNU Compiler Collection)
+        ///? fprintf(stderr, "Error opening file %s: %m\n", filename);
 
-		///* Print the error message from strerror() supplied by errno
-		fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
-		exit(1);
-	}
+        ///* Print the error message from strerror() supplied by errno
+        fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
+        exit(1);
+    }
 
-	///* Write the layer to the file
+    ///* Write the layer to the file
 
-	fwrite(layer, sizeof(Layer), 1, fp);
+    fwrite(layer, sizeof(Layer), 1, fp);
 
-	///* Close the file
-	fclose(fp);
+    ///* Close the file
+    fclose(fp);
 }
 
 ///* Load the layer from a binary file
 void load_layer_bin(Layer layer, const char *filename)
 {
-	FILE *fp = fopen(filename, "rb");
-	if (fp == NULL)
-	{
-		///* %m will print the error message from strerror() supplied by errno
-		///* Such as "No such file or directory"
-		///* %m is only supported by GCC (GNU Compiler Collection)
-		///? fprintf(stderr, "Error opening file %s: %m\n", filename);
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL)
+    {
+        ///* %m will print the error message from strerror() supplied by errno
+        ///* Such as "No such file or directory"
+        ///* %m is only supported by GCC (GNU Compiler Collection)
+        ///? fprintf(stderr, "Error opening file %s: %m\n", filename);
 
-		///* Print the error message from strerror() supplied by errno
-		fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
-		exit(1);
-	}
+        ///* Print the error message from strerror() supplied by errno
+        fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
+        exit(1);
+    }
 
-	///* Read the layer from the file
-	fread(layer, sizeof(Layer), 1, fp);
+    ///* Read the layer from the file
+    fread(layer, sizeof(Layer), 1, fp);
 
-	///* Close the file
-	fclose(fp);
+    ///* Close the file
+    fclose(fp);
 }
 
 ///* Clamp a value between a min and max value
 float clamp(int value, int min, int max)
 {
-	if (value < min)
-		return min;
+    if (value < min)
+        return min;
 
-	if (value > max)
-		return max;
+    if (value > max)
+        return max;
 
-	return value;
+    return value;
 }
 
 ///* Fill random rectangular area with a value
@@ -135,116 +150,206 @@ float clamp(int value, int min, int max)
 /// @param value, Value to fill the area with
 void fill_rectangle(Layer layer, int x, int y, int width, int height, float value)
 {
-	assert(width > 0);
-	assert(height > 0);
+    assert(width > 0);
+    assert(height > 0);
 
-	///* The area to fill is limited to the layer's dimensions
-	int x0 = clamp(x, 0, WIDTH - 1);
-	int y0 = clamp(y, 0, HEIGHT - 1);
-	int x1 = clamp(x0 + width - 1, 0, WIDTH - 1);
-	int y1 = clamp(y0 + height - 1, 0, HEIGHT - 1);
+    ///* The area to fill is limited to the layer's dimensions
+    int x0 = clamp(x, 0, WIDTH - 1);
+    int y0 = clamp(y, 0, HEIGHT - 1);
+    int x1 = clamp(x0 + width - 1, 0, WIDTH - 1);
+    int y1 = clamp(y0 + height - 1, 0, HEIGHT - 1);
 
-	///* Fill the area with the given value
-	for (int i = y0; i <= y1; ++i)
-	{
-		for (int j = x0; j <= x1; ++j)
-		{
-			layer[i][j] = value;
-		}
-	}
+    ///* Fill the area with the given value
+    for (int i = y0; i <= y1; ++i)
+    {
+        for (int j = x0; j <= x1; ++j)
+        {
+            layer[i][j] = value;
+        }
+    }
 }
 
 // fill circle
 void fill_circle(Layer layer, int center_x, int center_y, int radius, float value)
 {
-	assert(radius > 0);
-	assert(center_x >= 0);
-	assert(center_x < WIDTH);
-	assert(center_y >= 0);
-	assert(center_y < HEIGHT);
+    assert(radius > 0);
+    assert(center_x >= 0);
+    assert(center_x < WIDTH);
+    assert(center_y >= 0);
+    assert(center_y < HEIGHT);
 
-	///* The area to fill is limited to the layer's dimensions
-	int x0 = clamp(center_x - radius, 0, WIDTH - 1);
-	int y0 = clamp(center_y - radius, 0, HEIGHT - 1);
-	int x1 = clamp(center_x + radius, 0, WIDTH - 1);
-	int y1 = clamp(center_y + radius, 0, HEIGHT - 1);
+    ///* The area to fill is limited to the layer's dimensions
+    int x0 = clamp(center_x - radius, 0, WIDTH - 1);
+    int y0 = clamp(center_y - radius, 0, HEIGHT - 1);
+    int x1 = clamp(center_x + radius, 0, WIDTH - 1);
+    int y1 = clamp(center_y + radius, 0, HEIGHT - 1);
 
-	///* Fill the area with the given value
-	for (int i = y0; i <= y1; ++i)
-	{
-		for (int j = x0; j <= x1; ++j)
-		{
-			///* Calculate the distance from the center of the circle to the pixel (x, y)
-			///* and compare it to the radius of the circle to determine if the pixel is
-			///* inside the circle or not
-			///* (if the distance is less than the radius, the pixel is inside the circle)
-			float distance = sqrt(pow(i - center_y, 2) + pow(j - center_x, 2));
-			if (distance <= radius)
-			{
-				layer[i][j] = value;
-			}
-		}
-	}
+    ///* Fill the area with the given value
+    for (int i = y0; i <= y1; ++i)
+    {
+        for (int j = x0; j <= x1; ++j)
+        {
+            ///* Calculate the distance from the center of the circle to the pixel (x, y)
+            ///* and compare it to the radius of the circle to determine if the pixel is
+            ///* inside the circle or not
+            ///* (if the distance is less than the radius, the pixel is inside the circle)
+            float distance = sqrt(pow(i - center_y, 2) + pow(j - center_x, 2));
+            if (distance <= radius)
+            {
+                layer[i][j] = value;
+            }
+        }
+    }
 }
 
 ///* Transfer function
 float forward(Layer inputs, Layer weights)
 {
 
-	float output = 0.0f;
+    float output = 0.0f;
 
-	for (int y = 0; y < HEIGHT; ++y)
-	{
-		for (int x = 0; x < HEIGHT; ++x)
-		{
-			output += inputs[x][y] * weights[x][y];
-		}
-	}
+    for (int y = 0; y < HEIGHT; ++y)
+    {
+        for (int x = 0; x < HEIGHT; ++x)
+        {
+            output += inputs[x][y] * weights[x][y];
+        }
+    }
 
-	return output;
+    return output;
 }
 
-///* Generate random value between range min and max
-int random_int_range(int min, int max)
+/// @param inputs, Inputs to the network
+/// @param weights, Weights of the network
+void add_input_to_weight(Layer inputs, Layer weights)
 {
-	assert(min < max);
-	return min + rand() % (max - min + 1);
+    for (int y = 0; y < HEIGHT; ++y)
+    {
+        for (int x = 0; x < WIDTH; ++x)
+        {
+            weights[y][x] += inputs[y][x];
+        }
+    }
+}
+
+/// @param inputs, Inputs to the network
+/// @param weights, Weights of the network
+void subtract_input_from_weight(Layer inputs, Layer weights)
+{
+    for (int y = 0; y < HEIGHT; ++y)
+    {
+        for (int x = 0; x < WIDTH; ++x)
+        {
+            weights[y][x] -= inputs[y][x];
+        }
+    }
 }
 
 void random_rectangle_layer(Layer layer)
 {
 
-	int x = random_int_range(0, WIDTH - 2);			  ///* X coordinate of the top-left corner
-	int y = random_int_range(0, HEIGHT - 2);		  ///* Y coordinate of the top-left corner
-	int width = random_int_range(1, WIDTH - 1 - x);	  ///* Width of the rectangle
-	int height = random_int_range(1, HEIGHT - 1 - y); ///* Height of the rectangle
+    ///* Fill entire layer with 0
+    memset(layer, 0x0, sizeof(Layer));
 
-	fill_rectangle(layer, x, y, width, height, 1.0f);
+    int x = random_int_range(0, WIDTH - 1);       ///* X coordinate of the top-left corner
+    int y = random_int_range(0, HEIGHT - 1);      ///* Y coordinate of the top-left corner
+    int width = random_int_range(1, WIDTH - x);   ///* Width of the rectangle
+    int height = random_int_range(1, HEIGHT - y); ///* Height of the rectangle
+
+    fill_rectangle(layer, x, y, width, height, 1.0f);
 }
 
 void random_circle_layer(Layer layer)
 {
 
-	int center_x = random_int_range(BORDER_OFFSET_L_T, WIDTH - BORDER_OFFSET_R_B);	///* X coordinate of the center of the circle
-	int center_y = random_int_range(BORDER_OFFSET_L_T, HEIGHT - BORDER_OFFSET_R_B); ///* Y coordinate of the center of the circle
+    ///* Fill entire layer with 0
+    memset(layer, 0x0, sizeof(Layer));
 
-	///* Radius of the circle
-	///* Making sure that the circle is between the layer's dimensions
+    int center_x = random_int_range(BORDER_OFFSET_L_T, WIDTH - 1 - BORDER_OFFSET_R_B);  ///* X coordinate of the center of the circle
+    int center_y = random_int_range(BORDER_OFFSET_L_T, HEIGHT - 1 - BORDER_OFFSET_R_B); ///* Y coordinate of the center of the circle
 
-	///? int min_allowed_radius = (min(BORDER_OFFSET_L_T, BORDER_OFFSET_R_B) / 2) - 1;
-	int min_allowed_radius = 1;
-	int max_allowed_radius = min(WIDTH, HEIGHT) / 2;
+    ///* Radius of the circle
+    ///* Making sure that the circle is between the layer's dimensions
 
-	///* Minimum between center_x and center_y
-	max_allowed_radius = min(min(center_x, center_y), max_allowed_radius);
+    ///? int min_allowed_radius = (min(BORDER_OFFSET_L_T, BORDER_OFFSET_R_B) / 2) - 1;
+    int min_allowed_radius = 1;
+    int max_allowed_radius = min(WIDTH, HEIGHT) / 2;
 
-	///* Minimum between difference between center_x and center_y and the layer's dimensions
-	max_allowed_radius = min(min(WIDTH - center_x, HEIGHT - center_y), max_allowed_radius);
+    ///* Minimum between center_x and center_y
+    max_allowed_radius = min(min(center_x, center_y), max_allowed_radius);
 
-	///* Generate random radius between min and max radius
-	int radius = (min_allowed_radius != max_allowed_radius) ? random_int_range(min_allowed_radius, max_allowed_radius) : max_allowed_radius;
+    int radius = 1;
 
-	fill_circle(layer, center_x, center_y, radius, 1.0f);
+    if (max_allowed_radius > 1)
+    {
+        ///* Minimum between difference between center_x and center_y and the layer's dimensions
+        max_allowed_radius = min(min(WIDTH - center_x, HEIGHT - center_y), max_allowed_radius);
+
+        ///* Generate random radius between min and max radius
+
+        radius = (min_allowed_radius != max_allowed_radius) ? random_int_range(min_allowed_radius, max_allowed_radius + 1) : max_allowed_radius;
+    }
+
+    fill_circle(layer, center_x, center_y, radius, 1.0f);
+}
+
+int train_pass(Layer inputs, Layer weights)
+{
+
+    static char file_path[256];
+    static int count = 0;
+
+    int adjusted = 0;
+
+    for (int i = 0; i < NUMBER_OF_SAMPLES; ++i)
+    {
+        random_rectangle_layer(inputs);
+        if (forward(inputs, weights) > BIAS)
+        {
+            subtract_input_from_weight(inputs, weights);
+            snprintf(file_path, sizeof(file_path), SAMPLES_FOLDER_NAME "/ppm/" WEIGHT_SAMPLE_NAME "s/" WEIGHT_SAMPLE_NAME "-%03d.ppm", count++);
+            ///! printf("[INFO] saving %s\n", file_path);
+            save_layer_ppm(weights, file_path);
+            adjusted++;
+        }
+
+        random_circle_layer(inputs);
+
+        if (forward(inputs, weights) < BIAS)
+        {
+            add_input_to_weight(inputs, weights);
+            snprintf(file_path, sizeof(file_path), SAMPLES_FOLDER_NAME "/ppm/" WEIGHT_SAMPLE_NAME "s/" WEIGHT_SAMPLE_NAME "-%03d.ppm", count++);
+            ///! printf("[INFO] saving %s\n", file_path);
+            save_layer_ppm(weights, file_path);
+            adjusted++;
+        }
+    }
+
+    return adjusted;
+}
+
+int check_pass(Layer inputs, Layer weights)
+{
+
+    int adjusted = 0;
+
+    for (int i = 0; i < NUMBER_OF_SAMPLES; ++i)
+    {
+        random_rectangle_layer(inputs);
+        if (forward(inputs, weights) > BIAS)
+        {
+            adjusted++;
+        }
+
+        random_circle_layer(inputs);
+
+        if (forward(inputs, weights) < BIAS)
+        {
+            adjusted++;
+        }
+    }
+
+    return adjusted;
 }
 
 ///* Initialized as 0 if not initialized explicitly. (ISO C Standart)
@@ -254,79 +359,49 @@ static Layer weights;
 
 int main(void)
 {
+    ///* Create a folder for the sample
+    printf("[INFO] creating %s\n", SAMPLES_FOLDER_NAME);
+    if (mkdir(SAMPLES_FOLDER_NAME) < 0 && errno != EEXIST)
+    {
+        fprintf(stderr, "ERROR: could not create folder %s: %s", SAMPLES_FOLDER_NAME, strerror(errno));
+        exit(1);
+    }
 
-	///* Sample file name buffer
-	char filename[256];
+    ///* Changle the current working directory to the sample folder
+    chdir(SAMPLES_FOLDER_NAME);
 
-	///* Create a folder for the sample
-	struct stat st = {0};
+    printf("[INFO] creating %s\n", "ppm");
+    if (mkdir("ppm") < 0 && errno != EEXIST)
+    {
+        fprintf(stderr, "ERROR: could not create folder %s: %s", SAMPLES_FOLDER_NAME, strerror(errno));
+        exit(1);
+    }
 
-	if (stat(SAMPLES_FOLDER_NAME, &st) == -1)
-	{
-		mkdir(SAMPLES_FOLDER_NAME);
-	}
+    printf("[INFO] creating %s\n", "weights");
+    if (mkdir("ppm/weights") < 0 && errno != EEXIST)
+    {
+        fprintf(stderr, "ERROR: could not create folder %s: %s", SAMPLES_FOLDER_NAME, strerror(errno));
+        exit(1);
+    }
 
-	///* Changle the current working directory to the sample folder
-	chdir(SAMPLES_FOLDER_NAME);
+    chdir("..");
 
-	if (stat("bin", &st) == -1)
-	{
-		mkdir("bin");
-		mkdir("bin/rectangles");
-		mkdir("bin/circles");
-	}
-	if (stat("ppm", &st) == -1)
-	{
-		mkdir("ppm");
-		mkdir("ppm/rectangles");
-		mkdir("ppm/circles");
-	}
+    srand(CHECK_SEED_VALUE);
+    int adj = check_pass(inputs, weights);
+    printf("[INFO] fail rate of untrained model is %f\n", adj / (NUMBER_OF_SAMPLES * 2.0));
 
-	chdir("..");
+    for (int i = 0; i < FORWARD_TRAIN_PASSES; ++i)
+    {
+        srand(TRAIN_SEED_VALUE);
+        int adj = train_pass(inputs, weights);
+        printf("[INFO] Pass %d: adjusted %d times\n", i, adj);
+        if (adj <= 0)
+            break;
+    }
 
-#if defined(PREFIX)
-#undef PREFIX
-#endif // PREFIX
-#define PREFIX CIRCLE_SAMPLE_NAME
+    srand(CHECK_SEED_VALUE);
+    adj = check_pass(inputs, weights);
+    printf("[INFO] fail rate of trained model is %f\n", adj / (NUMBER_OF_SAMPLES * 2.0));
 
-	for (int i = 0; i < NUMBER_OF_SAMPLES; ++i)
-	{
-		printf("[Info] generating " PREFIX " %d\n", i);
-
-		///* Fill entire layer with 0
-		memset(inputs, 0x0, sizeof(Layer));
-
-		///* Generate random circle
-		random_circle_layer(inputs);
-
-		///* Save the layer to a binary and PPM file
-		snprintf(filename, sizeof filename, SAMPLES_FOLDER_NAME "/bin/" PREFIX "s/" PREFIX "-sample_%03d.bin", i);
-		save_layer_bin(inputs, filename);
-		snprintf(filename, sizeof filename, SAMPLES_FOLDER_NAME "/ppm/" PREFIX "s/" PREFIX "-sample_%03d.ppm", i);
-		save_layer_ppm(inputs, filename);
-	}
-
-#if defined(PREFIX)
-#undef PREFIX
-#endif // PREFIX
-#define PREFIX RECTANGLE_SAMPLE_NAME
-
-	for (int i = 0; i < NUMBER_OF_SAMPLES; ++i)
-	{
-		printf("[Info] generating " PREFIX " %d\n", i);
-
-		///* Fill entire layer with 0
-		memset(inputs, 0x0, sizeof(Layer));
-
-		///* Generate random rectangle
-		random_rectangle_layer(inputs);
-
-		///* Save the layer to a binary and PPM file
-		snprintf(filename, sizeof filename, SAMPLES_FOLDER_NAME "/bin/" PREFIX "s/" PREFIX "-sample_%03d.bin", i);
-		save_layer_bin(inputs, filename);
-		snprintf(filename, sizeof filename, SAMPLES_FOLDER_NAME "/ppm/" PREFIX "s/" PREFIX "-sample_%03d.ppm", i);
-		save_layer_ppm(inputs, filename);
-	}
-
-	return 0;
+    return 0;
 }
