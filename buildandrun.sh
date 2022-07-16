d791d1148f953623a5f83e94395756bafef16135
Detjon <detjon.mataj@gmail.com>
2022-07-16 02:29:59 +0200

    Update buildandrun.sh
    Removed unnecessary compiler flags which I used for debugging. Added compiler optimization level 3 flag.

--- buildandrun.sh
+++ buildandrun.sh
@@ -2,4 +2,7 @@
 
 set -xe
 
-gcc -Wall -Wextra -ggdb -lm -g3 -Wpedantic -pedantic -Wformat=2 -o main main.c && ./main 
+# gcc -Wall -Wextra -ggdb -lm -g3 -Wpedantic -pedantic -Wformat=2 main.c -o main
+gcc -O3 main.c -o main
+
+./main
